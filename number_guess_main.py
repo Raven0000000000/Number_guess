b6fef429d10985fb5cf8c7f48ed8634c8dcd2989
Nyako <rachel.rot@bk.ru>
2025-01-02 15:51:19 +0300

    Create number_guess_main.py
    Игра работоспособна. Можно еще усовершенствовать спросив хочет ли продолжить игрок игру или выйти.

---
+++ number_guess_main.py
@@ -0,0 +1,19 @@
+import random
+zagadannoe_chislo = random.randint(1,10)
+print(zagadannoe_chislo)
+i = 1
+while True:
+    if i <= 3:
+        vvod = int(input(f"Какое число я загадал?\nПопытка {i}:"))
+        if vvod > zagadannoe_chislo:
+            print("Я загадал число по меньше.")
+            i +=1
+        elif vvod < zagadannoe_chislo:
+            print ("Я загада л число поболше.")
+            i += 1
+        else:
+            print("Ты угадал - МОЛОДЕЦ!")
+            break
+    else:
+        print ("У тебя закончились попытки!")
+        break
